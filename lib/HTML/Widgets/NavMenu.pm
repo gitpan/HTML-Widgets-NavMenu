faed8c1996d80d25cd9b1da8bc6926330afa2441
Shlomi Fish <shlomif@shlomifish.org>
2009-12-09 09:49:24 +0000

    Import of SHLOMIF/HTML-Widgets-NavMenu-1.0400 from CPAN.
    gitpan-cpan-distribution: HTML-Widgets-NavMenu
    gitpan-cpan-version:      1.0400
    gitpan-cpan-path:         SHLOMIF/HTML-Widgets-NavMenu-1.0400.tar.gz
    gitpan-cpan-author:       SHLOMIF
    gitpan-cpan-maturity:     released

--- lib/HTML/Widgets/NavMenu.pm
+++ lib/HTML/Widgets/NavMenu.pm
@@ -3,13 +3,11 @@ use warnings;
 
 package HTML::Widgets::NavMenu;
 
-our $VERSION = '1.0302';
+our $VERSION = '1.0400';
 
 package HTML::Widgets::NavMenu::Error;
 
-use Error qw(:try);
-
-use base "Error";
+use base "HTML::Widgets::NavMenu::Object";
 
 package HTML::Widgets::NavMenu::Error::Redirect;
 
@@ -21,9 +19,9 @@ sub CGIpm_perform_redirect
 {
     my $self = shift;
 
-    my $q = shift;
+    my $cgi = shift;
 
-    print $q->redirect($q->script_name() . $self->{-redirect_path});
+    print $cgi->redirect($cgi->script_name() . $self->{-redirect_path});
     exit;
 }
 
@@ -174,7 +172,6 @@ package HTML::Widgets::NavMenu;
 use base 'HTML::Widgets::NavMenu::Object';
 
 use HTML::Widgets::NavMenu::Url;
-use Error qw(:try);
 
 require HTML::Widgets::NavMenu::Iterator::NavMenu;
 require HTML::Widgets::NavMenu::Iterator::SiteMap;
@@ -269,8 +266,12 @@ sub _register_path_info
 
     if (defined($redir_path))
     {
-        throw HTML::Widgets::NavMenu::Error::Redirect
-            -redirect_path => ($redir_path."/");
+        my $error = HTML::Widgets::NavMenu::Error::Redirect->new();
+
+        $error->{'-redirect_path'} = ($redir_path."/");
+        $error->{'msg'} = "Need to redirect";
+
+        die $error;
     }
 
     $path_info =~ s!^\/!!;
@@ -1027,7 +1028,7 @@ below under "The Node Description Component Class".
 =item 'nav_links_obj'
 
 This points to a hash reference whose keys are link IDs for
-the Firefox "Site Navigatoin Toolbar" 
+the Firefox "Site Navigation Toolbar" 
 ( L<http://www.bolwin.com/software/snb.shtml> ) and compatible programs,
 and its values are Node Description objects. (see "The Node Description 
 Class" below). Here's a sample code that renders the links as 
